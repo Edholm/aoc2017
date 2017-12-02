54cae28f18fcd5c185ab28f07b8756c8efc149ba
Emil Edholm <bobby@verypublic.org>
2017-12-02 13:06:27 +0100

    AOC stops at 25

--- src/main/kotlin/pub/edholm/aoc2017/utils/InputUtils.kt
+++ src/main/kotlin/pub/edholm/aoc2017/utils/InputUtils.kt
@@ -3,8 +3,8 @@ package pub.edholm.aoc2017.utils
 import java.net.URL
 
 fun getInputForDay(day: Int): String {
-  if (day < 1 || day > 24) {
-    throw IllegalArgumentException("Invalid day, git $day, expected 1<day<24")
+  if (day < 1 || day > 25) {
+    throw IllegalArgumentException("Invalid day, got $day, expected 1<day<25")
   }
   val resource: URL = {}.javaClass.enclosingClass.getResource("/day$day")
     ?: throw IllegalArgumentException("Input for day $day not found")
