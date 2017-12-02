fd009dc88e066f4f0e389e3bd063e2e35a646538
Emil Edholm <bobby@verypublic.org>
2017-12-02 10:11:13 +0100

    Day 2, part 2

--- src/main/kotlin/pub/edholm/aoc2017/day2/Checksum.kt
+++ src/main/kotlin/pub/edholm/aoc2017/day2/Checksum.kt
@@ -7,7 +7,8 @@ fun main(args: Array<String>) {
   val matrix = buildMatrix(inputForDay2)
   val checksum = Checksum()
   println("Day 2:")
-  println("  Part I:  ${checksum.calculate(matrix)}")
+  println("  Part I:  ${checksum.calculatePartOne(matrix)}")
+  println("  Part I:  ${checksum.calculatePartTwo(matrix)}")
 }
 
 private fun buildMatrix(input: String): List<List<Int>> {
@@ -21,9 +22,17 @@ private fun buildMatrix(input: String): List<List<Int>> {
 }
 
 internal class Checksum {
-  fun calculate(matrix: List<List<Int>>): Int {
-    return matrix.map { it.getMinAndMax() }
+  fun calculatePartOne(matrix: List<List<Int>>): Int {
+    return matrix
+      .map { it.getMinAndMax() }
       .map { it.diff() }
+      .sum()
+  }
+
+  fun calculatePartTwo(matrix: List<List<Int>>): Int {
+    return matrix
+      .map { it.findDivisible() }
+      .map { it.divide() }
       .sum()
   }
 
@@ -45,6 +54,21 @@ internal class Checksum {
     }
     return MinMax(min, max)
   }
+
+  private fun List<Int>.findDivisible(): Pair<Int, Int> {
+    this.forEachIndexed { index, value ->
+      this.subList(index + 1, this.size).forEach { nxtValue ->
+        if (value % nxtValue == 0) {
+          return Pair(value, nxtValue)
+        } else if (nxtValue % value == 0) {
+          return Pair(nxtValue, value)
+        }
+      }
+    }
+    throw IllegalStateException("No divisible numbers found in $this")
+  }
+
+  private fun Pair<Int, Int>.divide() = this.first / this.second
 }
 
 private data class MinMax(val min: Int, val max: Int)
--- src/test/kotlin/pub/edholm/aoc2017/day2/ChecksumTest.kt
+++ src/test/kotlin/pub/edholm/aoc2017/day2/ChecksumTest.kt
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test
 internal class ChecksumTest {
 
   @Test
-  fun calculate() {
+  fun calculatePartOne() {
     val input = listOf(listOf(5, 1, 9, 5), listOf(7, 5, 3), listOf(2, 4, 6, 8))
 
     val checksum = Checksum()
-    assertThat(checksum.calculate(input)).isEqualTo(18)
+    assertThat(checksum.calculatePartOne(input)).isEqualTo(18)
+
+  }
+
+  @Test
+  fun calculatePartTwo() {
+    val input = listOf(listOf(5, 9, 2, 8), listOf(9, 4, 7, 3), listOf(3, 8, 6, 5))
+
+    val checksum = Checksum()
+    assertThat(checksum.calculatePartTwo(input)).isEqualTo(9)
 
   }
 }
